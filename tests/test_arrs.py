356d087159880041b134c6b81388d44535fe58ab
Boris <borik1and@gmail.com>
2023-09-01 20:37:57 +0300

    added @pytest

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,11 +1,22 @@
+import pytest
+
 from utils import arrs
 
 
-def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
-    assert arrs.get([], 0, "test") == "test"
+@pytest.mark.parametrize("a, i, d, result", [([1, 2, 3], 1, "test", 2),
+                                             ([1, 2, 3], 0, "test", 1),
+                                             ([1, 2, 3], 2, "test", 3),
+                                             ([], 0, "test", "test")])
+def test_get(a, i, d, result):
+    assert arrs.get(a, i, d) == result
 
 
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+@pytest.mark.parametrize("a, b, c, result", [([1, 2, 3, 4], 1, 3, [2, 3]),
+                                             ([1, 2, 3, 4], 0, 3, [1, 2, 3])])
+def test_slice(a, b, c, result):
+    assert arrs.my_slice(a, b, c) == result
+    # assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
+    # assert arrs.my_slice([1, 2, 3, 4], 0, 3) == [1, 2, 3]
+    # assert arrs.my_slice([1, 2, 3], 0) == [1, 2, 3]
+    # assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    # assert arrs.my_slice([1, 2, 3], 2) == [3]
